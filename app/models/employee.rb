1ca35e182b2950a791073e0343c480ad7a55a433
Dennis Kamau <devjam.ke@gmail.com>
2024-01-27 15:47:44 +0300

    Add age calculation method to Employee model
    - Introduce an instance method  in the Employee model
    - Calculate the age based on the date_of_birth attribute
    - Adjust the age calculation to consider the birth month and day

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,18 @@ class Employee < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email address' }
   validates :contact_number, format: { with: /\A\d{10}\z/, message: 'should be 10 digits' }
+
+def age
+  if date_of_birth.present?
+    age = Time.now.year - date_of_birth.year
+    if Time.now.month < date_of_birth.month || (Time.now.month == date_of_birth.month && Time.now.day < date_of_birth.day)
+      age -= 1
+    end
+    age
+  else
+    nil
+  end
+
+end
+
 end
