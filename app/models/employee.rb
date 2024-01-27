657d8473626586a9471d9b28e74c8c7fda5c9a38
Dennis Kamau <devjam.ke@gmail.com>
2024-01-27 16:21:53 +0300

    Update age method to calculate age correctly.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,14 +7,11 @@ class Employee < ApplicationRecord
 
   # Get the age of the employee
   def age
-    if date_of_birth.present?
-      age = Time.now.year - date_of_birth.year
-      if Time.now.month < date_of_birth.month || (Time.now.month == date_of_birth.month && Time.now.day < date_of_birth.day)
-        age -= 1
-      end
-      age
-    else
-      nil
+    return nil unless date_of_birth.present?
+    age = Time.now.year - date_of_birth.year
+    if Time.now.month < date_of_birth.month || (Time.now.month == date_of_birth.month && Time.now.day < date_of_birth.day)
+      age -= 1
     end
+    age
   end
 end
